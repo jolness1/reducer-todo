a2c7f6b317bf61e7f55020b82c62fdee0f8701c9
Jacob Olness <jacobolness@Jacobs-MacBook-Pro.local>
2020-12-07 14:48:29 -0700

    set up reducer folder

---
+++ todo/src/reducers/index.js
@@ -0,0 +1,13 @@
+
+
+export const initialState = {
+    item: 'Learn about reducers',
+    completed: false,
+    id: 3892987589  
+}
+
+
+const reducer = (state, action) => {
+    switch(action.type)
+        
+}
